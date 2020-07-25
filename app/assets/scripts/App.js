88af1f24ddb6ae19ade08a9c8963ba9b2f281eee
Shekhar Atole <shekhar18.atole@gmail.com>
2020-07-25 14:27:59 +0530

    Completed all changes

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,25 @@ import MobileMenu from './MobileMenu';
 import RevealOnScroll from './RevealOnScroll';
 import StickyHeader from './StickyHeader';
 
-let stickyHeader = new StickyHeader();
-
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonials"), 60);
+new MobileMenu();
+let modal;
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == 'undefined') {
+      import(/* webpackChunkName: "modal" */ './Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20);
+      }).catch(() => console.log("There is a problem..!"))
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
 
 if (module.hot) {
   module.hot.accept()
